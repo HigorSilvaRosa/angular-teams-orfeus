957ab3c16126952827b68d0164309b61bc8f98db
Higor Silva Rosa <higor.s.rosa@gmail.com>
2017-06-20 19:54:40 -0300

    Rotas da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PeoplePageComponent } from './pages/people-page/people-page.component';
+import { TeamsPageComponent } from './pages/teams-page/teams-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'people',      component: PeoplePageComponent },
+  { path: 'teams',      component: TeamsPageComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PeoplePageComponent,
+    TeamsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
